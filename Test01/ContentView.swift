13a51ed355f71b35e56f79088249560f79bdd46e
muhammad4302001 <muhammad4302001@gmail.com>
2023-08-21 10:29:38 +0500

    Initial Commit

---
+++ Test01/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Test01
+//
+//  Created by Usermacmini1 on 21/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test01/Test01App.swift
@@ -0,0 +1,17 @@
+//
+//  Test01App.swift
+//  Test01
+//
+//  Created by Usermacmini1 on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
